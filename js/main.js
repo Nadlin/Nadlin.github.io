670a3fe47877d5ebc5f62d944619f5126c776b6e
Nadya Linnik <bolik2@list.ru>
2017-04-05 14:50:32 +0300

    data-img,-moz,-webkit

--- js/main.js
+++ js/main.js
@@ -67,4 +67,55 @@ jQuery(document).ready(function($) {
 		};
 	});
 		
+	
+	$.fn.dataImg = function(options) {
+
+  var settings = $.extend({
+    sml: 400,
+    med: 800,
+    lrg: 1450,
+    resize: false
+  }, options );
+
+  var elements = $(this);
+
+  function getSrc(element) {
+    var screen = $(window).width();
+    if (screen > settings.med) { 
+        return element.data('lrg');
+    }
+    else if (screen <= settings.med && screen > settings.sml) {
+        return element.data('med');
+    }
+    return element.data('sml');
+  }
+
+  function breakpoints() {
+    elements.each(function () {
+        var e = $(this);
+        var src = getSrc(e);
+        if(src != undefined){
+            if (e.is('img')) {
+                e.attr('src', src);
+            } 
+        }        
+    });
+  }breakpoints();
+
+  if(settings.resize == true){
+    $(window).resize(function(){
+      breakpoints();
+    });
+  }
+
+};
+$(function() {
+	$(".data-img").dataImg({
+	sml: 400,
+    med: 800,
+    lrg: 1450,
+    resize: true,
+	bgImg: true
+  });
+ });
 });
